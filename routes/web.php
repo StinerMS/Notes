27d187c5fa7d5540693fc5c7a9bbd7520970e992
Cid Monza <stinermonza@gmail.com>
2025-05-28 17:58:50 -0300

    Controller para Auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
+// Auth routes
+
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
 
