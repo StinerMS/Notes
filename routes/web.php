855ed81f9901b25db3aa7c8f0d511d6d15fa04a1
Cid Monza <stinermonza@gmail.com>
2025-05-28 16:57:56 -0300

    Limpeza do projeto - inicio da implementação do notes

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,4 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/about', function () {
-    echo '<h1>About Us</h1>';
-    echo '<p>This is the about page of our application.</p>';
-});
-
-Route::get('/main/{value}', [MainController::class, 'index']);
-
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-
-Route::get('/page3/{value}', [MainController::class, 'page3']);
