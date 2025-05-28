acda21f80cc56c9dc6102f071cc53124a3422098
Cid Monza <stinermonza@gmail.com>
2025-05-28 15:05:51 -0300

    criação de controllers e routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,4 +10,7 @@ Route::get('/', function () {
 Route::get('/about', function () {
     echo '<h1>About Us</h1>';
     echo '<p>This is the about page of our application.</p>';
+    echo 'The controller for the main controller is att ' . MainController::class;
 });
+
+Route::get('/main', [MainController::class, 'index']);
