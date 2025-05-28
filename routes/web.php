58456438ad34a006dcfd34d4627227dcef9fc96f
Cid Monza <stinermonza@gmail.com>
2025-05-28 14:08:45 -0300

    Introdução às Routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    echo '<h1>About Us</h1>';
+    echo '<p>This is the about page of our application.</p>';
+});
