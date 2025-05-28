225d9e6c893b4474ac817cbe8ee99eed1d40b63e
Cid Monza <stinermonza@gmail.com>
2025-05-28 15:15:25 -0300

    Criação de Routes e Controllers

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@ Route::get('/', function () {
 Route::get('/about', function () {
     echo '<h1>About Us</h1>';
     echo '<p>This is the about page of our application.</p>';
-    echo 'The controller for the main controller is att ' . MainController::class;
 });
 
 Route::get('/main', [MainController::class, 'index']);
