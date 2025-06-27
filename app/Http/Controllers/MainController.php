d2d3cd75bb9690790ab2134be2c1e333fdee3686
Cid Monza <stinermonza@gmail.com>
2025-06-27 14:12:48 -0300

    add relações entre models e pushando notes dos usuários no main controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class MainController extends Controller
 {
     public function index()
     {
         // load user's notes
+         $id = session('user.id');
+         $user = User::find($id)->toArray();
+         $notes = User::find($id)->notes()->get()->toArray();
+
+         echo '<pre>';
+         print_r($user);
+         print_r($notes);
+
+         die();
 
         //show home view
         return view('home');
