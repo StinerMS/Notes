04e74a132b6450c42dff67399fc6e54e9a8ed23b
Cid Monza <stinermonza@gmail.com>
2025-06-27 01:11:11 -0300

    middleware para quem já estiver logado

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,17 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIfNotLogged;
 use App\Http\Middleware\CheckIsLogged;
 use Illuminate\Support\Facades\Route;
 
-// Auth routes
+// Auth routes - user not logged
+Route::middleware([CheckIfNotLogged::class])->group(function(){
+    Route::get('/login', [AuthController::class, 'login']);
+    Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+});
 
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
-
+// app routes - user logged
 Route::middleware([CheckIsLogged::class])->group(function() {
     Route::get('/', [MainController::class, 'index']);
     Route::get('/novaNota', [MainController::class, 'newNote']);
