a7b42254284745d8c46934d3722c257a479a0a55
Cid Monza <stinermonza@gmail.com>
2025-06-27 13:49:10 -0300

    nomeando rotas e adicionando-as na pagina home

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ Route::middleware([CheckIfNotLogged::class])->group(function(){
 
 // app routes - user logged
 Route::middleware([CheckIsLogged::class])->group(function() {
-    Route::get('/', [MainController::class, 'index']);
-    Route::get('/novaNota', [MainController::class, 'newNote']);
-    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/', [MainController::class, 'index'])->name('home');
+    Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
 
