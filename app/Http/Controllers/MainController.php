2cc3413c9b517c326b7955e3fd75889d1c3426d3
Cid Monza <stinermonza@gmail.com>
2025-06-27 15:44:07 -0300

    desacoplando função que decripta o id para uma função privada do controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -27,22 +27,25 @@ class MainController extends Controller
 
     public function editNote($id)
     {
-        try {
-            $id = Crypt::decrypt($id);
-        } catch (DecryptException $e) {
-            return redirect()->route('home');
-        }
-
+        $id = $this->decryptId($id);
         echo "editing note with id = $id";
     }
 
-    public function deleteNote($id) {
+    public function deleteNote($id)
+    {
+        $id = $this->decryptId($id);
+        echo "deleting note with id = $id";
+    }
+
+    private function decryptId($id)
+    {
+        // check if id is encrypted
         try {
             $id = Crypt::decrypt($id);
         } catch (DecryptException $e) {
             return redirect()->route('home');
         }
 
-        echo "deleting note with id = $id";
+        return $id;
     }
 }
